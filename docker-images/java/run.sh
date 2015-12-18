69d2a18d4e9883fbed59dd7874bc251fc48cfc7c
Zeb McCorkle <zmccorkle@classcoder.com>
2015-12-18 14:30:24 -0600

    Add the ability for custom files

--- docker-images/java/run.sh
+++ docker-images/java/run.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+printenv | while read line; do
+  if [[ $line == SIGNUM_FILE_* ]]; then
+    IFS='=' read -ra SECTIONS <<< "$line"
+    IFS='_' read -ra FILENAME <<< "${SECTIONS[0]}"
+    FILENAME="${FILENAME[2]}"
+    unset SECTIONS[0]
+    printf "%s" "${SECTIONS[*]}" > "${FILENAME}"
+  fi
+done
 printf "%s" "$SIGNUM_CODE" > "$SIGNUM_CLASSNAME".java
 javac "$SIGNUM_CLASSNAME".java
 java "$SIGNUM_CLASSNAME"
--- docker-images/javascript/run.sh
+++ docker-images/javascript/run.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+printenv | while read line; do
+  if [[ $line == SIGNUM_FILE_* ]]; then
+    IFS='=' read -ra SECTIONS <<< "$line"
+    IFS='_' read -ra FILENAME <<< "${SECTIONS[0]}"
+    FILENAME="${FILENAME[2]}"
+    unset SECTIONS[0]
+    printf "%s" "${SECTIONS[*]}" > "${FILENAME}"
+  fi
+done
 printf "%s" "$SIGNUM_CODE" > index.js && npm install $SIGNUM_DEPS > /dev/null && node .
