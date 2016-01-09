6a3208074ef00b191d683445627d87cb6ed41f21
Zebulon McCorkle <zmccorkle@classcoder.com>
2016-01-10 00:26:42 +0100

    Add tests

---
+++ test/run.js
@@ -0,0 +1,25 @@
+require('../')
+
+const assert = require('better-assert')
+const request = require('request')
+
+describe('/run', function () {
+  describe('/java', function () {
+    it('should run a Java file correctly', function () {
+      request({
+        uri: 'http://localhost:8080/run/java',
+        method: 'POST',
+        json: true,
+        body: {
+          environment: {
+            'SIGNUM_CLASSNAME': 'HelloWorld',
+            'SIGNUM_CODE': 'class HelloWorld{public static void main(String[] args){System.out.println("Hello, world!");}}'
+          },
+          code: 'class HelloWorld{public static void main(String[] args){System.out.println("Hello, world!");}}'
+        }
+      }, function (err, res, body) {
+        assert(body === 'Hello, world!\n')
+      })
+    })
+  })
+})
