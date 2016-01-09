05aed8144d084ce5a401058333f24e12c86ec11b
Zebulon McCorkle <zmccorkle@classcoder.com>
2016-01-09 23:30:19 +0100

    Add a simple submission system

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const streams = require('memory-streams')
 const Language = require('./language')
 var app = express()
 
 const errors = require('./errors')
+
+var runs = []
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -12,6 +15,37 @@ app.use(function (req, res, next) {
 })
 
 app.use(bodyParser.json())
+
+app.post('/submit/:language', function (req, res, next) {
+  res.streamed = false
+  var language
+  try {
+    language = require('./languages/' + req.params.language)
+  } catch (e) {
+    return next(new errors[400]('signumd does not support language ' + req.params.language))
+  }
+  var stream = new streams.WritableStream()
+  stream.on('finish', function () {
+    runs.push(stream.toString())
+    res.end(JSON.stringify({
+      status: 'success',
+      data: {
+        output: stream.toString()
+      }
+    }))
+  })
+  language.run(req.body.environment, req.body.code, stream, next)
+})
+
+app.get('/submitted', function (req, res, next) {
+  res.streamed = false
+  res.end(JSON.stringify({
+    status: 'success',
+    data: {
+      runs: runs
+    }
+  }))
+})
 
 app.post('/run/:language', function (req, res, next) {
   res.streamed = true
