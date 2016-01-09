d87962915db9a88bf92a1d3252d237c45d573d05
Zebulon McCorkle <zmccorkle@classcoder.com>
2016-01-09 10:17:42 -0600

    Use an HTTP connection to Docker
    Instead of just calling Docker commands, this commit causes the app to use a
    faster and more reliable way of directly connecting to the Docker daemon and
    sending commands to it instead of through the CLI. This allows us to easily
    see what is happening when an image is created.
    
    This commit also adds a 5s timeout to any running containers, preventing CPU
    maxing out by students for a prolonged period of time.

--- languages/java.js
+++ languages/java.js
@@ -1,17 +1,28 @@
 const Language = require('../language')
-const spawn = require('child_process').spawn
 
-module.exports = new Language(function (environment, container, out) {
-  var args = ['run']
+const Dockerode = require('dockerode')
+const docker = new Dockerode()
+
+module.exports = new Language(function (environment, container, out, onErr) {
+  var env = []
   for (var variable in environment) {
     if (environment.hasOwnProperty(variable)) {
-      args.push('-e')
-      args.push(variable + '=' + environment[variable])
+      env.push(variable + '=' + environment[variable])
     }
   }
-  console.log(args)
-  args.push('signumc/signumd-runner:java')
-  var child = spawn('docker', args)
-  child.stdout.pipe(out)
-  child.stderr.pipe(out)
+  docker.createContainer({Image: 'signumc/signumd-runner:java', Cmd: '/app/run.sh', Tty: false, Env: env}, function (err, container) {
+    if (err != null) return onErr(err)
+    container.start(function (err, data) {
+      if (err != null) return onErr(err)
+    })
+    container.attach({stream: true, stdout: true, stderr: true}, function (err, stream) {
+      if (err != null) return onErr(err)
+      stream.pipe(out)
+    })
+    setTimeout(function () {
+      container.kill({}, function (err) {
+        if (err != null) return onErr(err)
+      })
+    }, 5000)
+  })
 })
