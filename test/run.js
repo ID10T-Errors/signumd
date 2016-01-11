3bfaaf8c8b74090b1b403581baec2479492df687
Zeb McCorkle <19mccorkle.zebulon@brenhamk-12.net>
2016-01-11 08:58:29 -0600

    Make sure the response has no extraneous characters

--- test/run.js
+++ test/run.js
@@ -21,5 +21,21 @@ describe('/run', function () {
         assert(body === 'Hello, world!\n')
       })
     })
+    it('should not return extraneous characters', function () {
+      request({
+        uri: 'http://localhost:8080/run/java',
+        method: 'POST',
+        json: true,
+        body: {
+          environment: {
+            'SIGNUM_CLASSNAME': 'HelloWorld',
+            'SIGNUM_CODE': 'class HelloWorld{public static void main(String[] args){System.out.println("Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!");}}'
+          },
+          code: 'class HelloWorld{public static void main(String[] args){System.out.println("Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!");}}'
+        }
+      }, function (err, res, body) {
+        assert(body === 'Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!\n')
+      })
+    })
   })
 })
